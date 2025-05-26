73fa906cc8e2d239871ea07128857c25526b8f99
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-05-26 17:26:49 +0900

    Feat: 플레이어 이동 및 카메라 이동

--- Assets/00_Scripts/PlayerMovement.cs
+++ Assets/00_Scripts/PlayerMovement.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5.0f;
+    public Vector3 cameraDir = Vector3.zero;
+    private Camera myCamera;
+    private Vector3 moveDir;
+
+    CharacterController controller;
+    private Animator animator;
+
+    private void Start()
     {
-        
+        myCamera = Camera.main;
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        Rotate();
+        Animate();
+        CameraMove();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector3(h, 0, v).normalized;
+
+        controller.SimpleMove(moveDir * moveSpeed);
+    }
+
+    void Rotate()
+    {
+        if (moveDir.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 10f * Time.deltaTime);
+        }
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("SPEED", moveDir.magnitude);
+    }
+
+    void CameraMove()
+    {
+        myCamera.transform.position = Vector3.Lerp(
+            myCamera.transform.position,
+            transform.position + cameraDir,
+            2.0f + Time.deltaTime);
     }
 }
